51dba3a284fc214ae2856e4734748b1bd658443e
Yoshitaka Fujii <yoshiyoshifujii@gmail.com>
2020-05-21 12:41:55 +0900

    wip

--- setting.sh
+++ setting.sh
@@ -1,18 +1,23 @@
 #!/bin/bash
 
+set -eu
+
+# shellcheck disable=SC2046
+cd $(dirname "$0") || exit
+
 ANSIBLE_PATH=~/workspace/settings/mac-ansible
 
 ## command line tools
-xcode-select --install
+#xcode-select --install
 
 ## install homebrew
-/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+#/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
 
 ## install ansible
 brew install ansible
 
 ## clone repo
-git clone -b template https://github.com/kohbis/mac-ansible.git ${ANSIBLE_PATH}
+git clone https://github.com/yoshiyoshifujii/mac-ansible.git ${ANSIBLE_PATH}
 
 ## ansible
 cd ${ANSIBLE_PATH}
